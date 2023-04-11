b965b63959e951c3f0cf58eccb8139f2d6551828
Shikuku39 <enhikuku39@gmail.com>
2023-04-11 21:15:11 +0300

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function closeNav() {
     document.querySelector('#nav-check').checked = false
 }
 function updateCharCount() {
-    var maxLength = 350
+    var maxLength = 200
     var currentLength = document.querySelector('#message').value.length
     var charCount = maxLength - currentLength
     document.querySelector('#char-count').innerHTML = charCount
